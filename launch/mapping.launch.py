08dcfeb705612f0f3c7166d44cf8fcba0b6ed2ac
NR-2716 <105415969+NR-2716@users.noreply.github.com>
2026-08-27 13:34:55 +0900

    Update mapping.launch.py

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -3,8 +3,9 @@ import os.path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 
 from launch_ros.actions import Node
@@ -12,6 +13,8 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     package_path = get_package_share_directory('fast_lio')
+    xacro_path = os.path.join(package_path, 'config', 'robot.urdf.xacro')
+    livox_path = get_package_share_directory('livox_ros_driver2')
     default_config_path = os.path.join(package_path, 'config')
     default_rviz_config_path = os.path.join(
         package_path, 'rviz', 'fastlio.rviz')
@@ -26,6 +29,7 @@ def generate_launch_description():
         'use_sim_time', default_value='false',
         description='Use simulation (Gazebo) clock if true'
     )
+
     declare_config_path_cmd = DeclareLaunchArgument(
         'config_path', default_value=default_config_path,
         description='Yaml config file path'
@@ -43,6 +47,36 @@ def generate_launch_description():
         description='RViz config file path'
     )
 
+    livox_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(livox_path, "launch_ROS2", 'rviz_MID360_launch.py'))
+    )
+
+    lidar_transform_node = Node(
+        package='g1_navigation',
+        executable='lidar_transformer',
+        name='lidar_transform',
+        output='screen'
+    )
+
+    tf2_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='map_to_odom_broadcaster',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom'],
+        output='screen'
+    )
+
+    robot_state_pub = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro', ' ', xacro_path])
+            }]
+    )
+
     fast_lio_node = Node(
         package='fast_lio',
         executable='fastlio_mapping',
@@ -50,6 +84,7 @@ def generate_launch_description():
                     {'use_sim_time': use_sim_time}],
         output='screen'
     )
+
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -58,12 +93,16 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
+    ld.add_action(livox_cmd)
+    ld.add_action(lidar_transform_node)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_config_path_cmd)
     ld.add_action(declare_config_file_cmd)
     ld.add_action(declare_rviz_cmd)
     ld.add_action(declare_rviz_config_path_cmd)
 
+    ld.add_action(tf2_node)
+    ld.add_action(robot_state_pub)
     ld.add_action(fast_lio_node)
     ld.add_action(rviz_node)
 
